6f0c5936a9a8d1dcb8575a056145b79b755c8a1e
sanok295 <sanok295@mail.ru>
2025-02-13 20:10:07 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+let isVisible = false;
+
+window.addEventListener('scroll', () => {
+  const hiddenWord = document.querySelector('.hidden-word');
+  const scrollPosition = window.scrollY;
+  const triggerHeight = 100; // Высота скролла, при которой произойдет анимация
+
+  if (scrollPosition > triggerHeight && !isVisible) {
+    hiddenWord.style.opacity = '1';
+    hiddenWord.style.transform = 'translateY(36px)';
+    isVisible = true; // Устанавливаем флаг, что слово теперь видно
+  }
+});
+
